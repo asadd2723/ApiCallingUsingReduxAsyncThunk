484a8185d037e7ae2f7bf055b9f4c8c12c25e2dd
asadd2723 <asadu3040@gmail.com>
2024-07-05 17:21:11 +0500

    add margin

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div className='bg-red-800 min-h-screen text-white flex flex-col items-center'>
     <h1 className='text-4xl pt-6 pb-7'>Api calling using Redux Async Thunk</h1>
     <h2 className='text-xl'>Click on button for Api Calling</h2>
-    <button className='bg-slate-50 text-red-900 mt-4 py-2 px-4 text-2xl font-semibold uppercase' onClick={()=>dispatch(fetchTodos())}>click here</button>
+    <button className='bg-slate-50 text-red-900 my-5 py-2 px-4 text-2xl font-semibold uppercase' onClick={()=>dispatch(fetchTodos())}>click here</button>
     {
       select.todo.data && select.todo.data.map((e)=>(
         <div key={e.id} className='p-3 flex items-center gap-3'>
